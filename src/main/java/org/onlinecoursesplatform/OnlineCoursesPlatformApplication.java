430f85ebce23c87fe7279965c38eee60a3201eaf
StanislavPB <perminovsb@gmail.com>
2024-02-23 22:48:24 +0000

    init commit

--- src/main/java/org/onlinecoursesplatform/OnlineCoursesPlatformApplication.java
+++ src/main/java/org/onlinecoursesplatform/OnlineCoursesPlatformApplication.java
@@ -3,7 +3,7 @@ package org.onlinecoursesplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class OnlineCoursesPlatformApplication {
 
     public static void main(String[] args) {
--- src/test/java/org/onlinecoursesplatform/OnlineCoursesPlatformApplicationTests.java
+++ src/test/java/org/onlinecoursesplatform/OnlineCoursesPlatformApplicationTests.java
@@ -3,7 +3,6 @@ package org.onlinecoursesplatform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OnlineCoursesPlatformApplicationTests {
 
     @Test
