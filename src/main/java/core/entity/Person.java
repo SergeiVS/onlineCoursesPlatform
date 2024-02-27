69c1ff8ef6be66172ba44a82c1c71c97f41141bd
valeb <valebog94@gmail.com>
2024-02-27 21:22:13 +0100

    Commit by Valeria, created Object "Person"

--- src/main/java/core/entity/Person.java
+++ src/main/java/core/entity/Person.java
@@ -1,4 +1,107 @@
 package core.entity;
 
+import java.util.Objects;
+
 public class Person {
+
+    private Integer Id;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+    private String courseName;
+    private String accessType;
+    private String status;
+
+    public Person(Integer id, String firstName, String lastName, String eMail, String courseName, String accessType, String status) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+        this.courseName = courseName;
+        this.accessType = accessType;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getAccessType() {
+        return accessType;
+    }
+
+    public void setAccessType(String accessType) {
+        this.accessType = accessType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(),
+                person.getFirstName()) && Objects.equals(getLastName(),
+                person.getLastName()) && Objects.equals(geteMail(), person.geteMail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), geteMail());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", eMail='" + eMail + '\'' +
+                ", courseName='" + courseName + '\'' +
+                ", accessType='" + accessType + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
