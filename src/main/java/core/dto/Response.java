b3b01fcf65861801be367b899189ed76a7816442
SergeiVS <fam.smirnoff@gmail.com>
2024-02-28 17:35:29 +0100

    New files for project planing.

--- src/main/java/core/dto/Response.java
+++ src/main/java/core/dto/Response.java
@@ -1,4 +1,32 @@
 package core.dto;
 
-public class Response {
+import core.dto.errors.ErrorCoding;
+
+import java.util.List;
+
+public class Response <T> {
+
+    private final T response;
+    private final List<ErrorCoding> errors;
+
+    public Response(T response, List<ErrorCoding> errors) {
+        this.response = response;
+        this.errors = errors;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+    public List<ErrorCoding> getErrors() {
+        return errors;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "response=" + response +
+                ", errors=" + errors +
+                '}';
+    }
 }
