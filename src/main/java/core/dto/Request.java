a65e8fe4fc18af1505253a40de07a29499013a3f
SergeiVS <fam.smirnoff@gmail.com>
2024-02-28 17:26:06 +0100

    New files for project planing.

--- src/main/java/core/dto/Request.java
+++ src/main/java/core/dto/Request.java
@@ -1,4 +1,21 @@
 package core.dto;
 
-public class Request {
+public class Request <T> {
+
+    private final T request;
+
+    public Request(T request) {
+        this.request = request;
+    }
+
+    public T getRequest() {
+        return request;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "request=" + request +
+                '}';
+    }
 }
