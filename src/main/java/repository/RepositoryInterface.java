ad2e4c3a7931d7958d3674a84ae6790738e039e0
valeb <valebog94@gmail.com>
2024-02-29 13:25:39 +0100

    Commit by Valeria, created interface for the repository

---
+++ src/main/java/repository/RepositoryInterface.java
@@ -0,0 +1,12 @@
+package repository;
+
+import java.util.List;
+
+public interface RepositoryInterface <T> {
+    List<T> objList();
+    void add(T obj);
+    void update(T obj);
+    List<T> findAll();
+    List<T> findByName(String name);
+
+}
