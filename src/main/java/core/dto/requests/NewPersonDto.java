1c9af5f007565507037ab1c8d112100267d088fd
SergeiVS <fam.smirnoff@gmail.com>
2024-02-29 17:51:47 +0100

    New files for project planing.

--- src/main/java/core/dto/requests/NewPersonDto.java
+++ src/main/java/core/dto/requests/NewPersonDto.java
@@ -8,17 +8,14 @@ public class NewPersonDto {
     private final String firstName;
     private final String lastName;
     private final String eMail;
-    private final String courseName;
-// courseName can be empty if user is not sure which course he/she is like to choose.
-private final Integer passwordHash;
-// passwordHash will be saved in separate repository as a value for key(emailHash)
+    private final Integer passwordHash;
+    // passwordHash will be saved in separate repository as a value for key(emailHash)
 
 
-    public NewPersonDto(String firstName, String lastName, String eMail, String courseName, Integer passwordHash) {
+    public NewPersonDto(String firstName, String lastName, String eMail, Integer passwordHash) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.eMail = eMail;
-        this.courseName = courseName;
         this.passwordHash = passwordHash;
     }
 
@@ -34,19 +31,16 @@ private final Integer passwordHash;
         return eMail;
     }
 
-    public String getCourseName() {
-        return courseName;
-    }
     public Integer getPasswordHash() {
         return passwordHash;
     }
+
     @Override
     public String toString() {
         return "NewPersonDto{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", eMail='" + eMail + '\'' +
-                ", courseName='" + courseName + '\'' +
                 '}';
     }
 }
