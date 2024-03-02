ec17333c41cbba42b8eb07be16e9448b9f895b82
SergeiVS <fam.smirnoff@gmail.com>
2024-03-02 19:57:48 +0100

    New files for project planing.

--- src/main/java/core/dto/responses/ResponseTestForPassing.java
+++ src/main/java/core/dto/responses/ResponseTestForPassing.java
@@ -1,4 +1,76 @@
 package core.dto.responses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResponseTestForPassing {
+    private final String testName;
+    private final List<ResponseQuestion> questions = new ArrayList<>();
+
+    public ResponseTestForPassing(String testName) {
+        this.testName = testName;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public List<ResponseQuestion> getQuestions() {
+        return questions;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseTestForPassing{" +
+                "testName='" + testName + '\'' +
+                ", questions=" + questions +
+                '}';
+    }
+
+    public class ResponseQuestion {
+        // нумерация вопросов происходит в сервисе который заполняет дто
+        private final int questionNumber;
+        private final String question;
+        private final String answerA;
+        private final String answerB;
+        private final String answerC;
+
+
+        public ResponseQuestion(int questionNumber, String question, String answerA, String answerB, String answerC) {
+
+            this.questionNumber = questionNumber;
+            this.question = question;
+            this.answerA = answerA;
+            this.answerB = answerB;
+            this.answerC = answerC;
+
+        }
+
+        public int getQuestionNumber() {
+            return questionNumber;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public String getAnswerB() {
+            return answerB;
+        }
+
+        public String getAnswerC() {
+            return answerC;
+        }
+
+        @Override
+        public String toString() {
+            return "ResponseQuestion{" +
+                    "questionNumber=" + questionNumber +
+                    ", question='" + question + '\'' +
+                    ", anmswerA='" + anmswerA + '\'' +
+                    ", answerB='" + answerB + '\'' +
+                    ", answerC='" + answerC + '\'' +
+                    '}';
+        }
+    }
 }
