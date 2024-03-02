1d30156ce089c024f35bad1d6d2c3adeeb7610de
SergeiVS <fam.smirnoff@gmail.com>
2024-03-02 08:01:24 +0100

    New files for project planing.

--- src/main/java/org/onlinecoursesplatform/Project1.java
+++
@@ -1,9 +0,0 @@
-package org.onlinecoursesplatform;
-
-public class Project1 {
-    public static void main(String[] args) {
-
-        System.out.println("TEST CONFLICT");
-        System.out.println("Conflict of Tests");
-    }
-}
