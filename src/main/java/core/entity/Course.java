049de4d98df5b8bc7a5112f8350feb67a406d0df
galeysh <galeysh@gmail.com>
2024-03-02 13:03:18 +0100

    Class Course Anastasia del setTest

--- src/main/java/core/entity/Course.java
+++ src/main/java/core/entity/Course.java
@@ -1,26 +1,27 @@
 package core.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Course {
 
-    private String name;
+    private  String name;
     private boolean isActive;
     private String material;
-    private List<String> tests;
+    private List<Test> tests;
 
     public Course(String name, boolean isActive, String material, List<String> tests) {
         this.name = name;
         this.isActive = isActive;
         this.material = material;
-        this.tests = tests;
+        this.tests = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-   public boolean isActive() {
+    public boolean isActive() {
         return isActive;
     }
 
@@ -36,11 +37,7 @@ public class Course {
         this.material = material;
     }
 
-    public List<String> getTests() {
+    public List<Test> getTests() {
         return tests;
-    }
-
-    public void setTests(List<String> tests) {
-        this.tests = tests;
     }
 }
