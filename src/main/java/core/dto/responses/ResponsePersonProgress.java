6ea1245488f1d2e16cd54d3cbd5ae92524802d1b
SergeiVS <fam.smirnoff@gmail.com>
2024-03-02 18:55:07 +0100

    New files for project planing.

---
+++ src/main/java/core/dto/responses/ResponsePersonProgress.java
@@ -0,0 +1,66 @@
+package core.dto.responses;
+
+import java.util.List;
+
+public class ResponsePersonProgress {
+
+    private final Integer id;
+    private final String firstName;
+    private final String lastName;
+    private final Integer courseId;
+    private final String courseName;
+    private final List<Integer> grades;
+    private final Integer progress;
+
+    public ResponsePersonProgress(Integer id, String firstName, String lastName, Integer courseId, String courseName,
+                                  List<Integer> grades, Integer progress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.grades = grades;
+        this.progress = progress;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public Integer getProgress() {
+        return progress;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponsePersonProgress{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                ", grades=" + grades +
+                ", progress=" + progress +
+                '}';
+    }
+}
