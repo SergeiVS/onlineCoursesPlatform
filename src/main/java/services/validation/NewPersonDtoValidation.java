a1f1937e8fe9d6e3dd574fa64c479ebcf41ce00f
SergeiVS <fam.smirnoff@gmail.com>
2024-03-06 20:31:10 +0100

    New files for project planing.

--- src/main/java/services/validation/NewPersonDtoValidation.java
+++ src/main/java/services/validation/NewPersonDtoValidation.java
@@ -7,8 +7,9 @@ import services.validation.exeptions.NullException;
 
 import java.util.List;
 
-public class NewPersonDtoValidation implements ValidationInterface <NewPersonDto> {
-EmailFormatValidation emailValidation = new EmailFormatValidation();
+public class NewPersonDtoValidation implements ValidationInterface<NewPersonDto> {
+    EmailFormatValidation emailValidation = new EmailFormatValidation();
+
     @Override
     public boolean validate(NewPersonDto newPersonDto, List<ErrorsDto> errors) {
         boolean isValid = true;
@@ -16,32 +17,22 @@ EmailFormatValidation emailValidation = new EmailFormatValidation();
             String fName = newPersonDto.getFirstName();
             String lName = newPersonDto.getLastName();
             String email = newPersonDto.geteMail();
-            Integer passHash = newPersonDto.getPasswordHash();
 
-            if (fName.isEmpty() || lName.isEmpty() || email.isEmpty()){
+            if (fName.isEmpty() || lName.isEmpty() || email.isEmpty()) {
                 errors.add(new ErrorsDto(ErrorCoding.E_400, "Fields: name and email can not be empty"));
                 isValid = false;
             }
-            if (emailValidation.validate(email, errors)){
-
-            }
-            if (passHash <= 0){
-                errors.add(new ErrorsDto(ErrorCoding.E_400, "Password can not be 0"));
+            if (!(emailValidation.validate(email, errors))) {
                 isValid = false;
             }
-        }catch (NumberFormatException e){
+
+        } catch (NumberFormatException e) {
             errors.add(new ErrorsDto(ErrorCoding.E_400, "Number input failed"));
             isValid = false;
-        }catch (NullException e){
+        } catch (NullException e) {
             errors.add(new ErrorsDto(ErrorCoding.E_400, "No null value alloyed"));
             isValid = false;
         }
-
-
-
-
-
-
         return isValid;
     }
 }
