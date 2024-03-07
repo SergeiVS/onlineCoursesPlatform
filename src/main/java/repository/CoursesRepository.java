0768d77fa8fd672b965581dea25c9f3b362ac374
galeysh <galeysh@gmail.com>
2024-03-07 21:53:16 +0100

    addCourse

--- src/main/java/repository/CoursesRepository.java
+++ src/main/java/repository/CoursesRepository.java
@@ -2,46 +2,46 @@ package repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class CoursesRepository<Course> implements Repository<Course> {
 
     private List<Course> course;
+    int idGenerator = 1;
 
     public CoursesRepository() {
         this.course = new ArrayList<>();
     }
     @Override
     public int idGenerator() {
-        int nextId = 1;
-        return nextId++;
+
+        return idGenerator++;
     }
 
     @Override
     public Integer addCourse(Course course){
-        int courseID = idGenerator();
-        course.setCourseId(courseID);
+        if (course.getCourseID() == 0){
+            course.setCourseId(idGenerator++);
+        }
         course.add(course);
-        return courseID;
     }
     @Override
     public List<Course> findAll(){
+
         return  new ArrayList<>(course);
     }
 
     @Override
     public Course finById(Integer courseId) {
-        for (Course course : courses) {
-            if (course.getCourseId().equals(courseId)){
-                return  course;
-            }
-        }
-
-        return null;
+        return course.stream().filter(course -> course.getCourseId().equals(courseId)).collect(Collectors.toList());
     }
 
     public List<Course> getCourse() {
+
         return course;
     }
 
