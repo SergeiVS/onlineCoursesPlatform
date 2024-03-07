74f9902c3c38d37f999a35c3e010dc0cfcec7952
anastasija <anastasija.mededa@tuhh.de>
2024-03-07 20:08:42 +0100

    AnastasijaM PersonRepository

--- src/main/java/repository/PersonRepository.java
+++ src/main/java/repository/PersonRepository.java
@@ -3,73 +3,43 @@ package repository;
 import core.entity.Person;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
-public class PersonRepository implements Repository <Person> {
-    private List<Person> persons;
-    private Map<Integer, Person> personMap;
-    private int idCounter;
-    public PersonRepository() {
-        this.persons = new ArrayList<>();
-        this.personMap = new HashMap<>();
-        this.idCounter = 1;
+public class PersonRepository implements Repository<Person> {
+    public List<Person> persons;
+    public PersonRepository(List<Person> persons) {
+        this.persons = persons;
+        this.idGenerator = 1;
     }
     @Override
-    public void add(Person person) {
-        int id = idGenerator();
-        person.setId(id);
-        persons.add(person);
-        personMap.put(id,person);
-        return id;
+    public List<Person> getAll() {
+        return persons;
     }
-    public List<Person> findAll() {
-        return new ArrayList<>(persons);
+    public void register(Person person) {
+        if (person.getPersonId() == 0) {
+            person.setPersonId(idGenerator++);
+        }
+        persons.add(person);
+    }
+    public void registerAll(List<Person> newPersons) {
+        for (Person person : newPersons) {
+            if (person.getPersonId() == 0) {
+                person.setPersonId(idGenerator++);
+            }
+        }
+        persons.addAll(newPersons);
     }
     public List<Person> findByName(String lastName) {
-        List<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getLastName().equals(lastName)) {
-                result.add(person);
-            }
-        }
-        return result;
+        return persons.stream().filter(person -> person.getLastName().equals(lastName)).collect(Collectors.toList());
     }
-    public Person findById(Integer id) {
-        return personMap.get(id);
+    public List<Person> findByEmail(String email) {
+        return persons.stream().filter(person -> person.getEmail().equals(email)).collect(Collectors.toList());
     }
-    public Person findByEmail(String email) {
-        for (Person person : persons) {
-            if (person.geteMail().equals(email)) {
-                return person;
-            }
-        }
-        return null;
-    }
-    public List<Person> findByCourse(String courseName) {
-        List<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getCourseName().contains (courseName)) {
-                result.add(person);
-            }
-        }
-        return result;
+    public List<Person> findByCourse(Integer CourseId) {
+        return persons.stream().filter(person -> person.getCourseId().equals(courseId)).collect(Collectors.toList());
     }
     public List<Person> findByAccessType(String accessType) {
-        List<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getAccessType().equals(accessType)) {
-                result.add(person);
-            }
-        }
-        return result;
-    }
-    private int idGenerator() {
-        return idCounter++;
-    }
-    public List<Person> getPersons() {
-        return persons;
+        return persons.stream().filter(person -> person.getAccessType().equals(accessType)).collect(Collectors.toList());
     }
 }
