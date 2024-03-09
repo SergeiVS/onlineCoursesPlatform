37aac801937f6a9fe24d3f2ac224ca228a1651b1
SergeiVS <fam.smirnoff@gmail.com>
2024-03-09 19:01:02 +0100

    Я вношу изменения в RepositoryInterface и CoursesRepository. В TestingService возникли ошибки. Я вношу изменения в виде комментариев.

--- src/main/java/repository/RepositoryInterface.java
+++ src/main/java/repository/RepositoryInterface.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface RepositoryInterface <T> {
     Integer add(T obj);
     List<T> findAll();
-    List<T> findById(Integer id);
+    T findById(Integer id);
     List<T> findByName(String name);
 
 
