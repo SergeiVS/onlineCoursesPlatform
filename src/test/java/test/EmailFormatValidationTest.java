93659d6898d418fd694d1722ae780f0d45cc2aff
SergeiVS <fam.smirnoff@gmail.com>
2024-03-11 18:29:26 +0100

    Новые сервисы

---
+++ src/test/java/test/EmailFormatValidationTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import core.dto.errors.ErrorsDto;
+import org.junit.jupiter.api.Test;
+import services.validation.EmailFormatValidation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailFormatValidationTest {
+
+    @Test
+    void validate() {
+        EmailFormatValidation validation = new EmailFormatValidation();
+        List<ErrorsDto> errors = new ArrayList<>();
+        String email = "hot@Test.com";
+        var isValid = validation.validate(email, errors);
+        assertTrue(isValid);
+    }
+    @Test
+    void validateFalseAt() {
+        EmailFormatValidation validation = new EmailFormatValidation();
+        List<ErrorsDto> errors = new ArrayList<>();
+        String email = "hotTest.com";
+        var isValid = validation.validate(email, errors);
+        assertFalse(isValid);
+    }
+    @Test
+    void validateFalseComma() {
+        EmailFormatValidation validation = new EmailFormatValidation();
+        List<ErrorsDto> errors = new ArrayList<>();
+        String email = "h,ot@Test.com";
+        var isValid = validation.validate(email, errors);
+        assertFalse(isValid);
+    }
+
+    @Test
+    void validateSymbols() {
+        EmailFormatValidation validation = new EmailFormatValidation();
+        List<ErrorsDto> errors = new ArrayList<>();
+        String email = "23-._hdfOLJm@Test.com";
+        var isValid = validation.validate(email, errors);
+        assertTrue(isValid);
+    }
+}
